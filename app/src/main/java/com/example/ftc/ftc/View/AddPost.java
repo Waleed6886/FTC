c3adc4767ac38c7c42ef0e76a89fd2615b0584c2
Ayoub Al Bader <ayoub@MacBook-Pro-alkhas-b-Ayoub.local>
2018-04-06 21:17:07 +0300

    Login page, add post page

---
+++ app/src/main/java/com/example/ftc/ftc/View/AddPost.java
@@ -0,0 +1,53 @@
+package com.example.ftc.ftc.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.example.ftc.ftc.R;
+
+public class AddPost extends AppCompatActivity {
+
+    private EditText mName;
+    private EditText mCategory;
+    private EditText mDescription;
+    private EditText mWorkingHours;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+
+        mName = findViewById(R.id.name);
+        mCategory = findViewById(R.id.category);
+        mDescription = findViewById(R.id.description);
+        mWorkingHours = findViewById(R.id.workingHours);
+
+        Button mImageGallery = findViewById(R.id.uploadImageGallery);
+        mImageGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 1);
+            }
+        });
+
+        Button mImageCamera = findViewById(R.id.uploadImageCamera);
+        mImageCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);
+            }
+        });
+
+    }
+
+}
